14c4ec7cfd004eaf443be199b33ab37a4d715d09
AdvayArgade <143066312+AdvayArgade@users.noreply.github.com>
2024-04-04 22:57:51 +0530

    add a print for the files list

--- app.py
+++ app.py
@@ -688,7 +688,7 @@ def text_to_doc(itinerary, input_dict):
 
     # Filter only the .docx files
     files_to_merge = [file for file in files_to_merge if file.endswith('.docx')]
-
+    st.write(files_to_merge)
     # Loop through the list
     for file in files_to_merge:
         # Construct the full file path
