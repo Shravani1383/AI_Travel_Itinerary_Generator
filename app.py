96b6cd63b7af83d23b0b8c6ec128e5f238c293b4
AdvayArgade <143066312+AdvayArgade@users.noreply.github.com>
2024-06-11 23:02:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -828,7 +828,7 @@ def text_to_doc(itinerary, input_dict):
 
     # Filter only the .docx files
     files_to_merge = [file for file in files_to_merge if file.endswith('.docx')]
-
+    files_to_merge.sort()
     # Loop through the list
     for file in files_to_merge:
         # Construct the full file path
